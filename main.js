c180b581843ee8bd850164e83de1fd263828fc3b
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-06-28 11:56:53 -0700

    refactored code; textarea can have single quotes, but not double quotes

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 var contactID = 0;
 
 $('form').submit(function(e) {
-    var formData = $(this).serializeArray();
-    var name = formData[0].value + " " + formData[1].value;
-    var description = formData[2].value;
+    var name = $('#first_name').val() + " " + $('#last_name').val();
+    var description = $('#description').val();
+    description = '\"'.concat(description, '\"');
 
     var btnHTML = "<button class='card btn btn-success btn-block' id='" + contactID + "' ";
-    btnHTML += "data-altP = '" + description + "'>";
+    btnHTML += "data-altP = " + description + ">";
     btnHTML += "<h2>" + name + "</h2>";
     btnHTML += "<p>Click for description!</p></button>";
     $('.current-ninjas').append(btnHTML);
     contactID++;
+
+    $('#first_name').val('');
+    $('#last_name').val('');
+    $('#description').val('');
+
     e.preventDefault();
 });
 
@@ -19,7 +24,7 @@ $(document).on('click', '.card', function() {
     var idP = '#' + contactID + ' p';
     var currentP = $(idP).text();
     $(idP).remove();
-    $(this).append('<p>' + $(this).attr('data-altP') +'</p>');
+    $(this).append("<p>" + $(this).attr('data-altP') +"</p>");
     $(this).attr('data-altP', currentP);
     if ($(this).hasClass('btn-info')) {
         $(this).removeClass('btn-info');
