340f3dd5f32b7970e5526daf92f91b035f80e560
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-06-27 20:36:42 -0700

    basic functionality working: submitting form produces card, clicking card flips it back and forth

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+var contactID = 0;
+
 $('form').submit(function(e) {
     var formData = $(this).serializeArray();
-    
+    var name = formData[0].value + " " + formData[1].value;
+    var description = formData[2].value;
+
+    var btnHTML = "<button class='card btn btn-info btn-block' id='" + contactID + "' ";
+    btnHTML += "data-altP = '" + description + "'>";
+    btnHTML += "<h2>" + name + "</h2>";
+    btnHTML += "<p>Click for description!</p></button>";
+    $('.current-ninjas').append(btnHTML);
+    contactID++;
     e.preventDefault();
+});
+
+$(document).on('click', '.card', function() {
+    var contactID = $(this).attr('id');
+    var idP = '#' + contactID + ' p';
+    var currentP = $(idP).text();
+    $(idP).remove();
+    $(this).append('<p>' + $(this).attr('data-altP') +'</p>');
+    $(this).attr('data-altP', currentP);
 });
